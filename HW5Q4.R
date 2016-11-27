47f9f29850f71c068b056a7405811f1ce9e6d2f0
Malcolm Taylor <malctaylor15@gmail.com>
2016-11-27 11:57:01 -0500

    Small changes
    Still need to check on Q3 and Q2

--- HW5Q4.R
+++ HW5Q4.R
@@ -8,9 +8,9 @@ plot(Returns, type = 'l')
 hist(Returns, breaks = 30)
 
 # Initialize parameters
-V <- 1e6
-alpha <- 0.95
-beta <- 0.025
+V <- 1e6 # Portfolio value 
+alpha <- 0.95 # Var Confidence 
+beta <- 0.025 # Estimate confidence 
 numb_sim <- 100000 # M 
 n <- length(Returns) # number of observations
 
@@ -19,7 +19,7 @@ n <- length(Returns) # number of observations
 Return_mean <- mean(Returns)
 Return_var <- var(Returns)
 norm_inverse <- qnorm((1-alpha), mean= 0, sd=1)
-Var_known = -V*(1 - exp(Return_mean +sqrt(Return_var)* norm_inverse))
+Var_known = V*(1 - exp(Return_mean +sqrt(Return_var)* norm_inverse))
 
 # Use slide 27 equations for upper and lower sigma paramter 
 conf_int_quantile_down<- beta/2
@@ -27,12 +27,12 @@ conf_int_quantile_up <- 1-beta/2
 conf_ints <- c(conf_int_quantile_down, conf_int_quantile_up)
 chisq_df <- n-1
 
-sigmas2_known <- ((n-1)*Return_var)/qchisq(p = conf_ints , df = chisq_df)
+sigmas2_known <- ((n-1)*Return_var)/qchisq(p = conf_ints , df = chisq_df) # stores 2 values, conf ints has both betas 
 #sigma_down <- ((n-1)*Return_var)/qchisq(p = conf_int_quantile_down, df = chisq_df) 
 
 # Watch the signs?
-Var_known_conf_int <- -V*(1 - exp(Return_mean +sqrt(sigmas2_known)* norm_inverse))
-# Var_down <- -V*(1 - exp(Return_mean +sqrt(sigma_down)* norm_inverse))
+Var_known_conf_int <- V*(1 - exp(Return_mean +sqrt(sigmas2_known)* norm_inverse)) # Again 2 sigmas (high, low) in sigmas2_known 
+# Var_down <- V*(1 - exp(Return_mean +sqrt(sigma_down)* norm_inverse))
 
 
 
@@ -49,13 +49,16 @@ chisq_MC <- rchisq(numb_sim, chisq_df)
 sigma2_MC <- ((n-1)*Return_var)/(chisq_MC)
 sigma_MC <- sqrt(sigma2_MC)
 
+# Preallocate space 
+mu_MC <- rep(0, numb_sim)
+
 # Simulate mean of the VaR 
 for(sim in 1:numb_sim){
-  mu_MC <- rnorm(1,mean = Return_mean, sd = (sigma_MC[sim]/sqrt(n)))
+  mu_MC[sim] <- rnorm(1,mean = Return_mean, sd = (sigma_MC[sim]/sqrt(n)))
 }
 
 # Simulate VaR alpha 
-VaR_alpha_MC = -V*(1 - exp(mu_MC +sigma_MC* norm_inverse))
+VaR_alpha_MC = V*(1 - exp(mu_MC +sigma_MC* norm_inverse))
 hist(VaR_alpha_MC, breaks = 40)
 VaR_MC_mean <- mean(VaR_alpha_MC)
 VaR_MC_conf_int <- quantile(VaR_alpha_MC,conf_ints)
@@ -68,4 +71,4 @@ Var_known_conf_int
 VaR_MC_mean
 VaR_MC_conf_int
 
-# The two methods are very similar. The means are slightly different but the confidence intervals overlap for the majority of the time. 
+# The two methods are very similar. The means are slightly different but the confidence intervals overlap for the majority of the time. The MC confidence interval is slightly larger than the analytical formula 
